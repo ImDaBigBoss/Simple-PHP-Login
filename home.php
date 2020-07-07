09f74e025a244b79ea39f2f2b58326ff159cfde4
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2020-07-07 20:24:18 +0200

    Update home.php
    Added documentation

--- home.php
+++ home.php
@@ -1,14 +1,14 @@
 <?php
 
-session_start();
+session_start(); //start the php session
 $all = "";
 
-if (isset($_POST["sent"]) == false) {
-  $all = $_SESSION["all"];
+if (isset($_POST["sent"]) == false) { //check if the request was sent form main page or not
+  $all = $_SESSION["all"]; //Get the data form previously stored variables
   $pass = explode(":", $all)[1];
   $user = explode(":", $all)[0];
 } else {
-  $pass = hash('sha512', $_POST["pass"]);
+  $pass = hash('sha512', $_POST["pass"]);//If it was, then log in/sign up with new details
   $user = $_POST["user"];
   
   $all = "$user:$pass";
@@ -16,9 +16,9 @@ if (isset($_POST["sent"]) == false) {
   $_SESSION["all"] = $all;
 }
 
-$err = 1;
-if ($_POST["action"] == "log") {
-  $found = false;
+$err = 1; //Set the defaumt error code
+if ($_POST["action"] == "log") {//If logging in
+  $found = false; //Set default to: account not found
   $directory = __DIR__  . "/accounts/";
   $files = array_diff(scandir($directory), array('..', '.'));
 
@@ -26,30 +26,30 @@ if ($_POST["action"] == "log") {
     $value2 = substr($value, 0, -4);
     $nall = explode(":", $all);
     
-    if ($value2 == $nall[0]) {
+    if ($value2 == $nall[0]) { //If the user corresponds with the current file
       $cont = file_get_contents(__DIR__  . "/accounts/" . $value);
-      if ($cont == $nall[1]) {
-        $found = true;
+      if ($cont == $nall[1]) { //Get if the password corresponds to the selected user
+        $found = true; //Yay, the user was found
       }
     }
   }
-} elseif ($_POST["action"] == "sign") {
-  $found = true;
+} elseif ($_POST["action"] == "sign") { //sign up
+  $found = true; //The user has to have been found as it was just created
   if (file_exists(__DIR__ . "/accounts/$user.txt")) {
-    $err = 2;
-    $found = false;
+    $err = 2; //User alredy exists
+    $found = false; //User not found: redirected to index.php
   } else {
-    file_put_contents(__DIR__  . "/accounts/$user.txt", $pass);
+    file_put_contents(__DIR__  . "/accounts/$user.txt", $pass); //Save the user
   }
 }
 
-if ($found == false) {
+if ($found == false) { //If the user was not found
   session_unset(); 
-  session_destroy();
+  session_destroy(); //remove all saved data
   
-  header("Location: index.php?e=$err");
+  header("Location: index.php?e=$err");//redirect to index.php with the selected error code
 
-  exit();
+  exit(); // extra security if the redirect fails
 }
 
 ?>
